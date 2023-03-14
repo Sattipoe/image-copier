19fdd4c4a780120f6e1b5a88bc96f226bbe5bb54
Sattipoe <91531023+Sattipoe@users.noreply.github.com>
2023-03-14 19:59:01 +0000

    removed and changed the tournament style selection and now reaching 0.92

--- image copier.py
+++ image copier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import copy
 import sys
 import random
 import random
@@ -44,7 +44,7 @@ def evaluate(solution):
     return (MAX - count) / MAX
 
 
-def mutate(solution, rate, add_rate=1, delete_rate=0.1, shift_rate=0.1, shuffle_rate=0.1):
+def mutate(solution, rate,elite_rate=0.1, add_rate=0.2, delete_rate=0.2, shift_rate=0.4, shuffle_rate=0.1):
     r = random.random()
     if r < add_rate:
         # add a new polygon
@@ -53,25 +53,19 @@ def mutate(solution, rate, add_rate=1, delete_rate=0.1, shift_rate=0.1, shuffle_
         # delete a random polygon
         del solution[random.randint(0, len(solution) - 1)]
     elif r < add_rate + delete_rate + shift_rate:
-        #random points of the polygon
+        # random points of the polygon
         polygon = random.choice(solution)
         for i in range(1, len(polygon)):
             if random.random() < rate:
                 x, y = polygon[i]
                 x += random.randint(-10, 10)
                 y += random.randint(-10, 10)
-                x = max(10, min(x, 190))
-                y = max(10, min(y, 190))
+                x = max(0, min(x, 200))
+                y = max(0, min(y, 200))
                 polygon[i] = (x, y)
     elif r < add_rate + delete_rate + shift_rate + shuffle_rate:
         # shuffle the order of polygons
-        random.shuffle(solution)
-    elif r < add_rate + delete_rate + shift_rate + shuffle_rate + 0.25:
-        polygon = random.choice(solution)
-        coords = [x for point in polygon[1:] for x in point]
-        coords = [x + (random.randint(-10, 10) if random.random() < rate else 0) for x in coords]
-        coords = [max(0, min(int(x), 200)) for x in coords]
-        polygon[1:] = list(zip(coords[::2], coords[1::2]))
+        polygon[x], polygon[j] = polygon[j], polygon[i]
     else:
         polygon = random.choice(solution)
         R = max(0, min(polygon[0][0] + random.randint(-30, 30), 255))
@@ -80,20 +74,12 @@ def mutate(solution, rate, add_rate=1, delete_rate=0.1, shift_rate=0.1, shuffle_
         A = max(0, min(polygon[0][3] + random.randint(-10, 10), 60))
         polygon[0] = (R, G, B, A)
 
+
     return solution
 
+
 def select(population):
-    # tournament size of 5
-    tournament_size = 5
-
-    # select two parents based on their fitness values
-    parents = []
-    for i in range(2):
-        tournament = random.sample(population, tournament_size)
-        winner = max(tournament, key=lambda x: x.fitness)
-        parents.append(winner)
-
-    return parents
+    return [copy.deepcopy(random.choice(population)) for i in range(2)]
 
 
 def combine(*parents):
@@ -113,11 +99,11 @@ def run(generations=500, population_size=100, seed=31):
     random.seed(seed)
 
     # initialization
-    population = Population.generate(initialise, evaluate, size=population_size, maximize=True)
+    population = Population.generate(initialise, evaluate, size=population_size, maximize=True,)
     population.evaluate()
-    evolution = (Evolution().survive(fraction=0.5)
-                 .breed(parent_picker=select, combiner=combine)
-                 .mutate(mutate_function=mutate, rate=0.1)
+    evolution = (Evolution().survive(n=1)
+                  .breed(parent_picker=select, combiner=combine)
+                 .mutate(mutate_function=mutate, rate=0.1, elite_rate=1)
                  .evaluate())
 
     best_solution = None
@@ -130,6 +116,7 @@ def run(generations=500, population_size=100, seed=31):
         if population.current_best.fitness > best_fitness:
             best_solution = population.current_best
             best_fitness = population.current_best.fitness
+            draw(population[0].chromosome).save("solution.png")
 
     return best_solution
 
